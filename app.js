d0caeb9141f8628c0145e96e02de946f089d5437
Moshiur Rahman Romel <mrahmannromel@gmail.com>
2025-07-03 14:44:29 +0600

    static commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
+const publicDirectory = path.join(__dirname, './public');
 // Using Middleware
-app.use(express.json());
+// app.use(express.json());
+app.use(express.static(publicDirectory));
+
+
+app.get('/quotes', (req, res)=>{
+    res.send({
+        name:'Jibananda',
+        quote:'Life is difficult'
+    });
+});
+
 
 module.exports = app;
